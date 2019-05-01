b9c3bb10c9464858d024cbfc90f9e35dfab284df
ttiedt <ttiedt@zagmail.gonzaga.edu>
2019-04-30 17:49:14 -0700

    update to style

---
+++ pages/order.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Atticus</title>
+  <link rel="stylesheet" href="../css/style.css" />
+</head>
+
+<body>
+  <div class="nav_div">
+    <nav>
+      <a href="../index.html">HOME</a> &nbsp | &nbsp
+      <a href="menu.html">MENU</a> &nbsp | &nbsp
+      <a href="gift_shop.html">GIFT SHOP</a> &nbsp | &nbsp
+      <a href="photo_gallery.html">PHOTO GALLERY</a> &nbsp | &nbsp
+      <a href="contact_us.html">CONTACT US</a>
+    </nav>
+  </div>
+  <div id="container">
+    <main>
+      <div>
+        <a href="index.html"><img src="../images/logo_text.png" alt="Atticus Logo" class="img_center" /></a><br />
+      </div>
+      
+
+    </main>
+  </div>
+</body>
+
+</html>
