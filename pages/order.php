42fc61737d6fa4919911bc4ee89726bbbec26695
ttiedt <ttiedt@zagmail.gonzaga.edu>
2019-05-05 17:21:57 -0700

    udates

--- pages/order.php
+++ pages/order.php
@@ -4,6 +4,7 @@
 <head>
   <title>Atticus</title>
   <link rel="stylesheet" href="../css/style.css" />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 </head>
 
 <body>
@@ -19,10 +20,41 @@
   <div id="container">
     <main>
       <div>
-        <a href="index.html"><img src="../images/logo_text.png" alt="Atticus Logo" class="img_center" /></a><br />
+        <a href="../index.html"><img src="../images/logo_text.png" alt="Atticus Logo" class="img_center" /></a><br/>
       </div>
-      
-
+      <?php
+        echo "<form action='receipt.php' method='POST'>
+          <div class='formheader'>
+            <h2>Online Order</h2>
+          </div>
+          <div class='textbox'>
+            <i class='far fa-user'></i>
+            <input name='name' type='text' placeholder='Name'>
+          </div>
+          <div class='textbox'>
+            <i class='far fa-envelope'></i>
+            <input name='email' type='email' placeholder='Email'>
+          </div>
+          <div class='textbox'>
+            <i class='fas fa-phone'></i>
+            <input name='phone' type='tel' placeholder='Phone'>
+          </div>
+          <div class='textbox'>
+            <i class='fas fa-mug-hot'></i>
+            <select name='order'>
+              <option value='coffee1'>Coffee1</option>
+              <option value='coffee2'>Coffee2</option>
+            </select>
+          </div>
+          <div class='radiodiv'>
+            <input name='size' type='radio' value='s'><lable>S</lable><br/>
+            <input name='size' type='radio' value='m'>M<br/>
+            <input name='size' type='radio' value='l'>L<br/>
+          </div>
+          <input type='submit' value='Submit' class='subbnt'>
+          <input type='reset' value='Cancel' class='subbnt'>
+        </form>";
+      ?>
     </main>
   </div>
 </body>
